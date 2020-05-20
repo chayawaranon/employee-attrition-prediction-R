e1970ce4ae1f986bc4fcef94ba749b8f4f808a6a
Hydrogen3105 <porinw2000@gmail.com>
2020-05-20 21:26:28 +0700

    test push

--- script.R
+++ script.R
@@ -3,7 +3,7 @@ library(tidyverse)
 #import data
 attritionData <- read.csv("./dataset/Employee-Attrition.csv")
 head(attritionData)
-
+tail(attritionData)
 #Data preparaing
 attritionData <- attritionData %>% select(-EmployeeCount,-EmployeeNumber)
 

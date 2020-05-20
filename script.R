3b25d5a907b79cd695dd41510a45a47928f405ff
cptwarman <48947748+cptwarman@users.noreply.github.com>
2020-05-20 21:33:12 +0700

    -

--- script.R
+++ script.R
@@ -1,9 +1,11 @@
 library(tidyverse)
+library(dplyr)
 
 #import data
 attritionData <- read.csv("./dataset/Employee-Attrition.csv")
 head(attritionData)
 tail(attritionData)
+
 #Data preparaing
 attritionData <- attritionData %>% select(-EmployeeCount,-EmployeeNumber)
 

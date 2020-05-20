c07e21c16ef9063b07fad7431d0e630dec21ca62
Hydrogen3105 <porinw2000@gmail.com>
2020-05-20 21:32:06 +0700

    delete tail

--- script.R
+++ script.R
@@ -3,7 +3,6 @@ library(tidyverse)
 #import data
 attritionData <- read.csv("./dataset/Employee-Attrition.csv")
 head(attritionData)
-tail(attritionData)
 #Data preparaing
 attritionData <- attritionData %>% select(-EmployeeCount,-EmployeeNumber)
 

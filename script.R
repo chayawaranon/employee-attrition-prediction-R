63b2b052d0bbdc3e189622a98e2455ebdf744115
Hydrogen3105 <porinw2000@gmail.com>
2020-05-20 23:17:43 +0700

    add data preparation

--- script.R
+++ script.R
@@ -1,9 +1,15 @@
 library(tidyverse)
-print('hello world')
-#import data
+library(dplyr)
+library(tidyr)
 
+#import data
+data <- read.csv('./dataset/HRDataset.csv')
 
 #Data preparaing
+preparedData <- data %>% filter(!is.na(EmpID)) %>% select(c(3:33))
+for(index in c(1:7,9,10,26)){
+  preparedData[,index] <- factor(preparedData[,index])
+}
 
 
 #Visualization

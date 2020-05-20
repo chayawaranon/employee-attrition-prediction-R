a8326f9e311137dcd6fa4a9fe7befeeeb1e8e38a
Hydrogen3105 <porinw2000@gmail.com>
2020-05-20 21:42:30 +0700

    modify script.R

--- script.R
+++ script.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 
 #import data
-attritionData <- read.csv("./dataset/Employee-Attrition.csv")
-head(attritionData)
+
+
 #Data preparaing
-attritionData <- attritionData %>% select(-EmployeeCount,-EmployeeNumber)
+
 
 #Visualization

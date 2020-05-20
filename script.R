66dbb0ea0ce78599d7608929fe6d8af24082bc20
thitiph0n <thitipontk@gmail.com>
2020-05-20 21:13:34 +0700

    prepare data

--- script.R
+++ script.R
@@ -1,5 +1,10 @@
 library(tidyverse)
+
 #import data
 attritionData <- read.csv("./dataset/Employee-Attrition.csv")
 head(attritionData)
 
+#Data preparaing
+attritionData <- attritionData %>% select(-EmployeeCount,-EmployeeNumber)
+
+#Visualization

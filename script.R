f31224ab14ee59bc2156ceb1d1c857084ec62cd4
thitiph0n <thitipontk@gmail.com>
2020-05-20 20:42:32 +0700

    import data

--- script.R
+++ script.R
@@ -1,13 +1,5 @@
 library(tidyverse)
 #import data
-attritionData <- read.csv("./dataset/WA_Fn-UseC_-HR-Employee-Attrition.csv")
 attritionData <- read.csv("./dataset/Employee-Attrition.csv")
 head(attritionData)
 
-
-<<<<<<< HEAD
-data <- read.csv("dataset/Employee-Attrition.csv", sep = ",")
-
-push eiei
-=======
->>>>>>> baed7ee090bf3f7e181a1fbcc6811581bcb47a4a

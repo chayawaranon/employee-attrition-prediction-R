f3ba424b386777c56e763068bf383273222f3105
cptwarman <48947748+cptwarman@users.noreply.github.com>
2020-05-21 00:53:01 +0700

    -

--- script.R
+++ script.R
@@ -1,23 +1,8 @@
-#import library
+#import data
 library(tidyverse)
-library(dplyr)
-<<<<<<< HEAD
-
-#import data
-
-attritionData <- read.csv("./dataset/Employee-Attrition.csv")
-head(attritionData)
-tail(attritionData)
-
-print('hello world')
-#import data
-
-=======
-library(tidyr)
 
 #import data
 data <- read.csv('./dataset/HRDataset.csv')
->>>>>>> c2650c56c9a7090e697df3d0b73112739168e8c4
 
 #Data preparaing
 

9dfa4eccb7d624da67d93051b8ea6128c97fa58e
cptwarman <48947748+cptwarman@users.noreply.github.com>
2020-05-20 20:43:14 +0700

    -

--- script.R
+++ script.R
@@ -4,9 +4,8 @@ attritionData <- read.csv("./dataset/WA_Fn-UseC_-HR-Employee-Attrition.csv")
 head(attritionData)
 
 
-<<<<<<< HEAD
 data <- read.csv("dataset/Employee-Attrition.csv", sep = ",")
 
-push eiei
-=======
->>>>>>> baed7ee090bf3f7e181a1fbcc6811581bcb47a4a
+555555
+
+
